a1298742f1f4226f47d5b5ad089aff6cb07c7b55
shahednur <shahednurctg@gmail.com>
2017-08-03 02:18:33 +0600

    Product category file

---
+++ model/category_db.php
@@ -0,0 +1,34 @@
+<?php 
+class Category{
+    public function getCategorys(){
+        $db = Database::getDB();
+        
+        $query = "SELECT * FROM categories ORDER BY categoryID";
+        try{
+            $statement = $db->query($query);
+            $statement->execute();
+            $result = $statement->fetchAll();
+            $statement->closeCursor();
+            return $result;
+        }catch(PDOException $e){
+            display_db_error($e->getMessage());
+        }
+    }
+    
+    public function getCategory($category_id){
+        $db = Database::getDB();
+        
+        $query = "SELECT * FROM categories
+                  WHERE categoryID = '$category_id'";
+        try{
+            $statement = $db->query($query);
+            $statement->execute();
+            $result = $statement->fetch();
+            $statement->closeCursor();
+            return $result;
+        }catch(PDOException $e){
+            display_db_error($e->getMessage());
+        }
+    }
+}
+?>
